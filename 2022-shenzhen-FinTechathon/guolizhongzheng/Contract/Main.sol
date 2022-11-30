83546e7e0cc68263534e52b94b56eebaf89e75e6
lyjqklxtc <102032920+lyjqklxtc@users.noreply.github.com>
2022-11-30 12:25:25 +0800

    Create Main.sol

---
+++ 2022-shenzhen-FinTechathon/guolizhongzheng/Contract/Main.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.25;
+pragma experimental ABIEncoderV2;
+import "./Doctor.sol";
+import "./Patient.sol";
+contract Main is Patient,Doctor{
+
+
+
+
+
+
+
+
+}
+//患者地址:
+//0x93Ff8fe9BF40051E8763C864B15A0E87f2f96468
+
+//医生地址:
+//0x5C56f3c44675f4D91B28411743C530A0B8bBC319
+
+//待就诊的电子病历ID:
+//0xa2ea04271c5cD6cf2392ffe921515E258F7Dc978
