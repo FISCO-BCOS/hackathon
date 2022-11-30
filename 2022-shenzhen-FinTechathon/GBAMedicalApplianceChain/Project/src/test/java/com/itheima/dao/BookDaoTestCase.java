924ac394b803fbfcc0a69dc8da1820abc302c3d1
pascalria <116817813+pascalria@users.noreply.github.com>
2022-11-30 11:54:34 +0800

    Add files via upload

---
+++ 2022-shenzhen-FinTechathon/GBAMedicalApplianceChain/Project/src/test/java/com/itheima/dao/BookDaoTestCase.java
@@ -0,0 +1,89 @@
+package com.itheima.dao;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookDaoTestCase {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testGetById(){
+        System.out.println(bookDao.selectById(1));
+    }
+
+    @Test
+    void testSave(){
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookDao.insert(book);
+    }
+
+    @Test
+    void testUpdate(){
+        Book book = new Book();
+        book.setId(17);
+        book.setType("测试数据abcdefg");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookDao.updateById(book);
+    }
+
+    @Test
+    void testDelete(){
+        bookDao.deleteById(16);
+    }
+
+    @Test
+    void testGetAll(){
+        bookDao.selectList(null);
+    }
+
+    @Test
+    void testGetPage(){
+        IPage page = new Page(2,5);
+        bookDao.selectPage(page, null);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        System.out.println(page.getRecords());
+    }
+
+    @Test
+    void testGetBy(){
+        QueryWrapper<Book> qw = new QueryWrapper<>();
+        qw.like("name","Spring");
+        bookDao.selectList(qw);
+    }
+
+    @Test
+    void testGetBy2(){
+        String name = "1";
+        LambdaQueryWrapper<Book> lqw = new LambdaQueryWrapper<Book>();
+        //if(name != null) lqw.like(Book::getName,name);
+        lqw.like(name != null,Book::getName,name);
+        bookDao.selectList(lqw);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 2022-shenzhen-FinTechathon/GBAMedicalApplianceChain/Project/src/test/java/com/itheima/service/BookServiceTest.java
@@ -0,0 +1,61 @@
+package com.itheima.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private IBookService bookService;
+
+    @Test
+    void testGetById(){
+        System.out.println(bookService.getById(4));
+    }
+
+    @Test
+    void testSave(){
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookService.save(book);
+    }
+
+    @Test
+    void testUpdate(){
+        Book book = new Book();
+        book.setId(17);
+        book.setType("-----------------");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookService.updateById(book);
+    }
+
+    @Test
+    void testDelete(){
+        bookService.removeById(18);
+    }
+
+    @Test
+    void testGetAll(){
+        bookService.list();
+    }
+
+    @Test
+    void testGetPage(){
+        IPage<Book> page = new Page<Book>(2,5);
+        bookService.page(page);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        System.out.println(page.getRecords());
+    }
+
+}
