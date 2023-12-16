f7dc869e87da376594d8e64b1805ddf85419b93a
Belowkm <93333275+Belowkm@users.noreply.github.com>
2023-12-16 10:22:45 +0800

    Add files via upload

---
+++ 2023-shenzhen-FinTechathon/WeTender/Contract/Enterprise.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.25;
+pragma experimental ABIEncoderV2;
+
+import "./Project.sol";
+
+contract Enterprise
+{
+    address etp_addr;           //企业地址
+    string public etp_id;       //企业编号
+    string public etp_name;     //企业名称
+    uint public num;            //企业项目数量
+    
+    mapping(uint => address) public proj;       //企业项目列表
+    
+    modifier onlyOwner() 
+    {
+        require(msg.sender == etp_addr, "无权限!");
+        _;
+    }
+    
+    constructor(string id, string name) public
+    {
+        etp_addr = msg.sender;
+        etp_id = id;
+        etp_name = name;
+        num = 0;
+    }
+    
+    //开标同步
+    function AddProj(uint ProjectID, address ProjectAddr, string remark) external
+    {
+        proj[ProjectID] = ProjectAddr;
+        num++;
+    }
+    
+    //施工
+    function Build(uint ProjectID, uint money, string remark) public onlyOwner()
+    {
+        Project(proj[ProjectID]).Build(money, remark);
+    }
+    
+    //竣工
+    function Finish(uint ProjectID, string remark) public onlyOwner()
+    {
+        Project(proj[ProjectID]).Finish(remark);
+        num--;
+    }
+}
