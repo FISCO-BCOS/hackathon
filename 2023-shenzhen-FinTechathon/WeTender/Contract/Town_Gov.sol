9a5d0bcebcb7c63950ab5e59c15e319f81e68195
Belowkm <93333275+Belowkm@users.noreply.github.com>
2023-12-16 23:59:31 +0800

    Delete 2023-shenzhen-FinTechathon/WeTender/Contract directory

--- 2023-shenzhen-FinTechathon/WeTender/Contract/Town_Gov.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.25;
-pragma experimental ABIEncoderV2;
-
-import "./Government.sol";
-
-contract Town_Gov is Government
-{
-    constructor(string name) public Government(name) {}
-}
